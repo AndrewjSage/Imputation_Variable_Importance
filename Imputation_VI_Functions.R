f542b47b2d74116d9d93c36806570023f713084b
Andrew Sage <andrewsage@Andrews-MBP.attlocal.net>
2019-07-24 23:03:15 -0500

    updated imputation functions according to new rfsrc options

--- Imputation_VI_Functions.R
+++ Imputation_VI_Functions.R
@@ -74,71 +74,106 @@ miceVI <- function(x,y,ntreesimp, ntrees, xvar){
   }
 
 
-
-Impute_and_VI <- function(data, ntreesimp=300, ntrees=500, nmult=5, ntechs=6, xvar){
+#Function to delete values and perform imputation using each technique in question. 
+Impute_and_VI <- function(data, ntreesimp=300, ntrees=500, nmult=5, ntechs=9, xvar){
   #separate predictor variables from response since some techniques require one or other
   x <- data[,-ncol(data)]
   y <- data[,ncol(data)]
   #setup dataframe to store variable importance results
   VI <- array(NA, dim=c(ncol(x), ntechs))  #rows correspond to variables, columns to imputation techniques
   
-  #Impute using rfImpute
+  #Strawman-median imputation
+  tech <- 1
   if(sum(is.na(x))>0){
-    Imputed <- rfImpute(x, y, iter=5, ntree=ntreesimp)[,c(2:(ncol(x)+1),1)]} else{  #reordered so y still last
+    Imputed <- data
+      Imputed[which(is.na(Imputed[,xvar])), xvar] <- median(Imputed[, xvar], na.rm=TRUE)} else{  #reordered so y still last
+      Imputed <- data
+      }
+  rfMiss <- rfsrc(y~., data=Imputed, ntree=ntrees,importance=TRUE)
+  if(is.factor(y)){VI[,tech]<-rfMiss$importance[,1]}else{VI[,tech]<-rfMiss$importance} #If y is a factor, permutation importance is given in 1st col. Otherwise just a vector
+  
+  
+  #Impute using rfImpute
+  tech <- 2
+    if(sum(is.na(x))>0){
+      Imputed <- rfImpute(x, y, iter=5, ntree=ntreesimp)[,c(2:(ncol(x)+1),1)]} else{  #reordered so y still last
       Imputed <- data
     }
-  rfMiss <- randomForest(y~., data=Imputed, ntree=ntrees,importance=TRUE)
-  #store variable importance resulting from RFimpute in first column  
-  if(is.factor(y)){VI[,1]<-rfMiss$importance[,3]}else{VI[,1]<-rfMiss$importance[,1]} #If y is a factor, permutation importance is given in 3rd col. Otherwise first
-     
+  rfMiss <- rfsrc(y~., data=Imputed, ntree=ntrees,importance=TRUE)
+  if(is.factor(y)){VI[,tech]<-rfMiss$importance[,1]}else{VI[,tech]<-rfMiss$importance} #If y is a factor, permutation importance is given in 1st col. Otherwise just a vector
+  
   #Impute using missForest
+  tech <- 3
   if(sum(is.na(x))>0){
-    ImputedX <- missForest(x, maxiter=10, ntree=ntreesimp, verbose=F)$ximp
+    ImputedX <- impute(data = x, mf.q = 1/ncol(x))
     Imputed <- cbind(ImputedX, y)} else{  
       Imputed <- data
-    }
-  rfMiss <- randomForest(y~., data=Imputed, ntree=ntrees,importance=TRUE)
-  if(is.factor(y)){VI[,2]<-rfMiss$importance[,3]}else{VI[,2]<-rfMiss$importance[,1]} #If y is a factor, permutation importance is given in 3rd col. Otherwise first
+    }  
+  rfMiss <- rfsrc(y~., data=Imputed, ntree=ntrees,importance=TRUE)
+  if(is.factor(y)){VI[,tech]<-rfMiss$importance[,1]}else{VI[,tech]<-rfMiss$importance} #If y is a factor, permutation importance is given in 1st col. Otherwise just a vector
   
   #Impute using RFSRC-1 iteration
+  tech <- 4
   rfMiss <- rfsrc(y~., data=data, ntree=ntrees,importance=c("permute"), na.action=c("na.impute"), nimpute=1)
-  if(is.factor(y)){VI[,3]<-rfMiss$importance[,1]}else{VI[,3]<-rfMiss$importance} #If y is a factor, permutation importance is given in 1st col. Otherwise just a vector
+  if(is.factor(y)){VI[,tech]<-rfMiss$importance[,1]}else{VI[,tech]<-rfMiss$importance} #If y is a factor, permutation importance is given in 1st col. Otherwise just a vector
 
-  
   #Impute using RFSRC-5 iterations
+  tech <- 5
   rfMiss <- rfsrc(y~., data=data, ntree=ntrees,importance=c("permute"), na.action=c("na.impute"), nimpute=5)
-  if(is.factor(y)){VI[,4]<-rfMiss$importance[,1]}else{VI[,4]<-rfMiss$importance} #If y is a factor, permutation importance is given in 1st col. Otherwise just a vector
+  if(is.factor(y)){VI[,tech]<-rfMiss$importance[,1]}else{VI[,tech]<-rfMiss$importance} #If y is a factor, permutation importance is given in 1st col. Otherwise just a vector
+  
+  # RFSRC unsupervised - 1 iteration
+  tech <- 6  
+  if(sum(is.na(x))>0){
+    ImputedX <- impute(data = x, nimpute = 1)
+    Imputed <- cbind(ImputedX, y)} else{  
+    Imputed <- data
+    }
+  rfMiss <- randomForest(y~., data=Imputed, ntree=ntrees,importance=TRUE)
+  if(is.factor(y)){VI[,tech]<-rfMiss$importance[,3]}else{VI[,tech]<-rfMiss$importance[,1]} #If y is a factor, permutation importance is given in 3rd col. Otherwise first
+  
+  # RFSRC unsupervised - 5 iterations
+  tech <- 7
+  if(sum(is.na(x))>0){
+    ImputedX <- impute(data = x, nimpute = 5)
+    Imputed <- cbind(ImputedX, y)} else{  
+    Imputed <- data
+    }
+  rfMiss <- randomForest(y~., data=Imputed, ntree=ntrees,importance=TRUE)
+  if(is.factor(y)){VI[,tech]<-rfMiss$importance[,3]}else{VI[,tech]<-rfMiss$importance[,1]} #If y is a factor, permutation importance is given in 3rd col. Otherwise first
   
   #Impute using CALIBER
   #since this is a multiple imputation technique, perform nmult times then average VI
+  tech <- 8
   VImat <- replicate(n=nmult, CaliberVI(x,y, ntreesimp = ntreesimp, ntrees=ntrees, xvar=xvar))
-  VI[,5] <- rowMeans(VImat)
+  VI[,tech] <- rowMeans(VImat)
   
   #Impute using mice
   #since this is a multiple imputation technique, perform nmult times then average VI
+  tech <- 9
   VImat <- replicate(n=nmult, miceVI(x,y,ntreesimp = ntreesimp, ntrees=ntrees, xvar=xvar))
-  VI[,6] <- rowMeans(VImat)
+  VI[,tech] <- rowMeans(VImat)
   
   return(VI)  
 }
 
 
 #Function to do deletion and imputation. Apply this for different xvars after data have been generated
-Del_Impute <- function(data, xvar, pvec, ntrees=500){
-  Deleted_Data <- lapply(X=pvec, data=data, FUN=DeleteMissing, xvar=xvar)
+Del_Impute <- function(data, xvar, pvec, ntrees=500, missingness){
+  Deleted_Data <- lapply(X=pvec, data=data, FUN=DeleteMissing, xvar=xvar, missingness=missingness)
   VI <- lapply(X=Deleted_Data, FUN=Impute_and_VI, xvar=xvar)
   return(VI)
 }
 
-#function to generated data, then delete and impute for all variables of interest.
-Gen_Del_Impute <- function(rho, xvarvec, pvec, size=100, ntrees=500){
+#function to generated data, then delete and impute for all variables of interest and measure VI
+Gen_Del_Impute <- function(rho, xvarvec, pvec, size=100, ntrees=500, missingness="MCAR"){
   DATA <- Generate_Sim_Data(rho, size)
-  VI <- lapply(X=xvarvec, pvec=pvec, data=DATA, FUN=Del_Impute)
+  VI <- lapply(X=xvarvec, pvec=pvec, data=DATA, FUN=Del_Impute, missingness=missingness)
   return(VI)
 }
 
-#function to generated data, then delete and impute for all variables of interest.
-Del_Impute_wrapper <- function(data, xvarvec, pvec, ntrees=500){
-  VI <- lapply(X=xvarvec, pvec=pvec, data=data, FUN=Del_Impute)
+#function to delete and impute for all variables of interest for given dataset and measure VI
+Del_Impute_wrapper <- function(data, xvarvec, pvec, ntrees=500, missingness="MCAR"){
+  VI <- lapply(X=xvarvec, pvec=pvec, data=data, FUN=Del_Impute, missingness=missingness)
   return(VI)
 }
