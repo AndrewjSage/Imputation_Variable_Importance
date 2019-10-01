4c163a51ca05c102a0caa099e13abad66a7227d5
andrewjsage <ajsage@gmail.com>
2019-10-01 11:52:10 -0500

    Updated code for graphics

--- Process_Results.R
+++ Process_Results.R
@@ -83,41 +83,156 @@ g_legend<-function(a.gplot){
   legend <- tmp$grobs[[leg]]
   return(legend)}
 
-#################################################################
+####################################################################################
 
-#x1 deleted and imputed using MAR
-sim <- 1
-xvar <- 1
-xvarused <- c(1,3,5)
-ylower <- 0
-yupper <- 0.5
-type="MCAR"
+#Function to create graphic for all variables with a given missingness level
+Create_Combined_Graphic <- function(type, sim, xvar, xvarused, ylower, yupper){
+  #type is "MAR, MNAR, MCAR"
+  #sim 1, 2, "BH", or "STEM"
+  #xvar is variable deleted and imputed
+  #xvarused is the vector of variables deleted and imputed in the simulation should always be c(1,3,5) 
+  #ylower and yupper are bounds for y-axis
+  
+  rho <- 0
+  load(paste("sim",sim,"_rho_",rho,"_",type,".Rdata", sep = ""))
+  A <- SummaryFunc(MVVIMP)
+  dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=which(xvarused==xvar))
+  p1 <- Createplot(dfList, xvar=xvar)+ theme(legend.position = "bottom")+ylim(c(ylower,yupper))+ggtitle(expression(paste(rho,"=0")))
+  
+  rho <- 25
+  load(paste("sim",sim,"_rho_",rho,"_",type,".Rdata", sep = ""))
+  A <- SummaryFunc(MVVIMP)
+  dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=which(xvarused==xvar))
+  p2 <- Createplot(dfList, xvar=xvar)+ theme(legend.position = "bottom")+ylim(c(ylower,yupper))+ggtitle(expression(paste(rho,"=0.25")))
+  
+  rho <- 50
+  load(paste("sim",sim,"_rho_",rho,"_",type,".Rdata", sep = ""))
+  A <- SummaryFunc(MVVIMP)
+  dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=which(xvarused==xvar))
+  p3 <- Createplot(dfList, xvar=xvar)+ theme(legend.position = "bottom")+ylim(c(ylower,yupper))+ggtitle(expression(paste(rho,"=0.5")))
+  
+  rho <- 75
+  load(paste("sim",sim,"_rho_",rho,"_",type,".Rdata", sep = ""))
+  A <- SummaryFunc(MVVIMP)
+  dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=which(xvarused==xvar))
+  p4 <- Createplot(dfList, xvar=xvar)+ theme(legend.position = "bottom")+ylim(c(ylower,yupper))+ggtitle(expression(paste(rho,"=0.75")))
+  
+  #Put plots together
+  
+  mylegend<-g_legend(p1)
+  p <- grid.arrange(arrangeGrob(p1 + theme(legend.position="none"),
+                                p2 + theme(legend.position="none"),
+                                p3 + theme(legend.position="none"),
+                                p4 + theme(legend.position="none"),
+                                nrow=2),
+                    mylegend, nrow=2,heights=c(10, 1))
+  return(p)
+}
 
-rho <- 0
-load(paste("sim",sim,"_rho_",rho,"_",type,".Rdata", sep = ""))
+#####################################################################################
+
+#Simulation 1 - x1 deleted/imputed
+Create_Combined_Graphic(type="MCAR", sim=1, xvar=1, xvarused=c(1,3,5), ylower=0, yupper=0.5)
+Create_Combined_Graphic(type="MAR", sim=1, xvar=1, xvarused=c(1,3,5), ylower=0, yupper=0.5)
+Create_Combined_Graphic(type="MNAR", sim=1, xvar=1, xvarused=c(1,3,5), ylower=0, yupper=0.5)
+
+#Simulation 1 - x3 deleted/imputed
+Create_Combined_Graphic(type="MCAR", sim=1, xvar=3, xvarused=c(1,3,5), ylower=0, yupper=0.3)
+Create_Combined_Graphic(type="MAR", sim=1, xvar=3, xvarused=c(1,3,5), ylower=0, yupper=0.3)
+Create_Combined_Graphic(type="MNAR", sim=1, xvar=3, xvarused=c(1,3,5), ylower=0, yupper=0.3)
+
+#Simulation 1 - x5 deleted/imputed
+Create_Combined_Graphic(type="MCAR", sim=1, xvar=5, xvarused=c(1,3,5), ylower=0, yupper=0.25)
+Create_Combined_Graphic(type="MAR", sim=1, xvar=5, xvarused=c(1,3,5), ylower=0, yupper=0.25)
+Create_Combined_Graphic(type="MNAR", sim=1, xvar=5, xvarused=c(1,3,5), ylower=0, yupper=0.25)
+
+#Simulation 2 - x1 deleted/imputed
+Create_Combined_Graphic(type="MCAR", sim=2, xvar=1, xvarused=c(1,3,5), ylower=0, yupper=0.5)
+Create_Combined_Graphic(type="MAR", sim=2, xvar=1, xvarused=c(1,3,5), ylower=0, yupper=0.5)
+Create_Combined_Graphic(type="MNAR", sim=2, xvar=1, xvarused=c(1,3,5), ylower=0, yupper=0.5)
+
+#Simulation 2 - x3 deleted/imputed
+Create_Combined_Graphic(type="MCAR", sim=2, xvar=3, xvarused=c(1,3,5), ylower=0, yupper=0.3)
+Create_Combined_Graphic(type="MAR", sim=2, xvar=3, xvarused=c(1,3,5), ylower=0, yupper=0.3)
+Create_Combined_Graphic(type="MNAR", sim=2, xvar=3, xvarused=c(1,3,5), ylower=0, yupper=0.3)
+
+#Simulation 2 - x5 deleted/imputed
+Create_Combined_Graphic(type="MCAR", sim=2, xvar=5, xvarused=c(1,3,5), ylower=0, yupper=0.25)
+Create_Combined_Graphic(type="MAR", sim=2, xvar=5, xvarused=c(1,3,5), ylower=0, yupper=0.25)
+Create_Combined_Graphic(type="MNAR", sim=2, xvar=5, xvarused=c(1,3,5), ylower=0, yupper=0.25)
+
+############################################################################
+
+#Plot Showing all variables together
+
+load("sim1_rho_75_MCAR.Rdata")
+
 A <- SummaryFunc(MVVIMP)
-dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=which(xvarused==xvar))
-p1 <- Createplot(dfList, xvar=xvar)+ theme(legend.position = "bottom")+ylim(c(ylower,yupper))+ggtitle(expression(paste(rho,"=0")))
+dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=2)
+df <- do.call("rbind", dfList)
+#df <- filter(df, Method%in%c("missForest", "CALIBER", "rfmice"))
+p <- ggplot(df, aes(x=p, y=VI, colour = factor(var, labels=c("X1", "X2", "X3", "X4", "X5", "X6"))))+theme(text = element_text(size=16))+ facet_wrap(~Method)
+dodge <- position_dodge(width=0.05)
+p1 <- p +geom_line(aes(group = var)) +
+  geom_errorbar(aes(ymin = Lower, ymax = Upper), position = dodge, width = 0.05)+ labs(x = "Proportion of X5 Missing", y="Scaled Importance")+ theme(plot.title = element_text(hjust = 0.5))+ylim(c(0,0.4))+ labs(colour='Variable') 
+p1
 
-rho <- 25
-load(paste("sim",sim,"_rho_",rho,"_",type,".Rdata", sep = ""))
+
+#################################################################################
+#Delete/Impute Simulation Using Boston Housing
+load("MVVIMP_BH_MCAR.Rdata")
+xvarused <- c(1,4,6,7)
+
 A <- SummaryFunc(MVVIMP)
-dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=which(xvarused==xvar))
-p2 <- Createplot(dfList, xvar=xvar)+ theme(legend.position = "bottom")+ylim(c(ylower,yupper))+ggtitle(expression(paste(rho,"=0.25")))
+dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=1)
+p1 <- Createplot(dfList, xvar=xvarused[1])+ theme(legend.position = "bottom")+ylim(c(0, .1))+ggtitle("Crime Rate")
+p1
 
-rho <- 50
-load(paste("sim",sim,"_rho_",rho,"_",type,".Rdata", sep = ""))
 A <- SummaryFunc(MVVIMP)
-dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=which(xvarused==xvar))
-p3 <- Createplot(dfList, xvar=xvar)+ theme(legend.position = "bottom")+ylim(c(ylower,yupper))+ggtitle(expression(paste(rho,"=0.5")))
+dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=2)
+p2 <- Createplot(dfList, xvar=xvarused[2])+ theme(legend.position = "bottom")+ylim(c(0, .1))+ggtitle("Borders Charles River")
+p2
 
-rho <- 75
-load(paste("sim",sim,"_rho_",rho,"_",type,".Rdata", sep = ""))
 A <- SummaryFunc(MVVIMP)
-dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=which(xvarused==xvar))
-p4 <- Createplot(dfList, xvar=xvar)+ theme(legend.position = "bottom")+ylim(c(ylower,yupper))+ggtitle(expression(paste(rho,"=0.75")))
+dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=3)
+p3 <- Createplot(dfList, xvar=xvarused[3])+ theme(legend.position = "bottom")+ylim(c(0, .3))+ggtitle("Avg. Number of Rooms")
+p3
 
-#Put plots together
+A <- SummaryFunc(MVVIMP)
+dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=4)
+p4 <- Createplot(dfList, xvar=xvarused[4])+ theme(legend.position = "bottom")+ylim(c(0, .1))+ggtitle("Age")
+p4
+
+mylegend<-g_legend(p1)
+p <- grid.arrange(arrangeGrob(p1 + theme(legend.position="none"),
+                              p2 + theme(legend.position="none"),
+                              p3 + theme(legend.position="none"),
+                              p4 + theme(legend.position="none"),
+                              nrow=2),
+                  mylegend, nrow=2,heights=c(10, 1))
+p
+
+
+#################################################################################
+#Delete/Impute Simulation Using STEM
+load("MVVIMP_STEM_MCAR.Rdata")
+xvarused <- c(1,2,6,14)
+
+A <- SummaryFunc(MVVIMP)
+dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=1)
+p1 <- Createplot(dfList, xvar=xvarused[1])+ theme(legend.position = "bottom")+ylim(c(0, .25))+ggtitle("x1")
+
+A <- SummaryFunc(MVVIMP)
+dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=2)
+p2 <- Createplot(dfList, xvar=xvarused[2])+ theme(legend.position = "bottom")+ylim(c(0, .35))+ggtitle("x2")
+
+A <- SummaryFunc(MVVIMP)
+dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=3)
+p3 <- Createplot(dfList, xvar=xvarused[3])+ theme(legend.position = "bottom")+ylim(c(0, .35))+ggtitle("x3")
+
+A <- SummaryFunc(MVVIMP)
+dfList <- lapply(X=1:5, FUN=Summarydf, Array=A, xvar=4)
+p4 <- Createplot(dfList, xvar=xvarused[4])+ theme(legend.position = "bottom")+ylim(c(0, .2))+ggtitle("x4")
 
 mylegend<-g_legend(p1)
 p <- grid.arrange(arrangeGrob(p1 + theme(legend.position="none"),
